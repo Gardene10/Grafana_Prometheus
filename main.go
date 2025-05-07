7cbeacc98b78b9ce3a3036fc4f3b1b9739ac4909
gardene10 <gardene.v.c@gmail.com>
2025-05-07 15:15:37 -0300

    feat: create Go app with HTTP request counter and simulated online users metric using Prometheus

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"math/rand"
+	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -17,15 +17,38 @@ var onlineUsers = prometheus.NewGauge(prometheus.GaugeOpts{
 	},
 })
 
+var httpRequests = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "goapp_http_requests_total",
+		Help: "Total HTTP requests",
+	},
+	[]string{"method", "path"},
+),
+
+var httpDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Name: "goapp_http_request_duration",
+	Help: "Duration in seconds of all HTTP requests",
+}, []string{"handler"})
+
 func main() {
 	r := prometheus.NewRegistry()
 	r.MustRegister(onlineUsers)
+	r.MustRegister(httpRequests)
+	r.MustRegister(httpDuration)
 
 	go func() {
 		for {
 			onlineUsers.Set(float64(rand.Intn(2000)))
 		}
 	}()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		// Incrementa a métrica com as labels corretas
+		httpRequests.WithLabelValues(req.Method, req.URL.Path).Inc()
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, World!"))
+	})
 
 	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
 	log.Fatal(http.ListenAndServe(":8181", nil))
